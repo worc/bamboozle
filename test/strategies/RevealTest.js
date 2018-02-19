23a45e5e13ffa171950d2b154b1af3e8dc6763ba
worc <curtis.s.mcallister@gmail.com>
2018-02-18 16:00:37 -0800

    add leftToRight tests, reformat to 4-space indents

--- test/strategies/RevealTest.js
+++ test/strategies/RevealTest.js
@@ -1,28 +1,61 @@
 import assert from 'assert';
 import Reveal from '../../src/strategies/Reveal.js';
 
-describe('Reveal', function() {
-  describe('#oneBitAndShuffleUntilDone', function() {
-    it('should return a bitmap of the same length, when all bits are zeroes', function() {
-      let testArray = [0, 0, 0];
-      let generator = Reveal.oneBitAndShuffleUntilDone(testArray); 
-      assert.equal(generator.next().value.length, 3);
-    });
-    
-    it('should return done: true when all bits are zeroes', () => {
-      let testArray = [0, 0, 0];
-      let generator = Reveal.oneBitAndShuffleUntilDone(testArray);
-      assert(generator.next().done);
+describe('Reveal', function () {
+    describe('#oneBitAndShuffleUntilDone', () => {
+        it('should return a bitmap of the same length, when all bits are zeroes', () => {
+            let testArray = [0, 0, 0];
+            let generator = Reveal.oneBitAndShuffleUntilDone(testArray);
+            assert.equal(generator.next().value.length, 3);
+        });
+
+        it('should return done: true when all bits are zeroes', () => {
+            let testArray = [0, 0, 0];
+            let generator = Reveal.oneBitAndShuffleUntilDone(testArray);
+            assert(generator.next().done);
+        });
+
+        it('should yield an array with one less one on each step', () => {
+            let testArray = [1, 1, 1];
+            let generator = Reveal.oneBitAndShuffleUntilDone(testArray);
+
+            assert(generator.next().value.join('').match(/1/g).length === 2, 'first yield with 2 ones');
+            assert(generator.next().value.join('').match(/1/g).length === 1, 'second yield with 1 ones');
+            assert(generator.next().value.join('').match(/1/g) === null, 'third and last return with 0 ones');
+        });
     });
 
-    it('should yield an array with one less one on each step', () => {
-      let testArray = [1, 1, 1];
-      let generator = Reveal.oneBitAndShuffleUntilDone(testArray);
-      
-      assert(generator.next().value.join('').match(/1/g).length === 2, 'first yield with 2 ones');
-      assert(generator.next().value.join('').match(/1/g).length === 1, 'second yield with 1 ones');
-      assert(generator.next().value.join('').match(/1/g) === null, 'third and last return with 0 ones');
-    });  
-  });
+    describe('#leftToRight', () => {
+        it('should return an empty array when given an empty array', () => {
+            let testArray = [];
+            let generator = Reveal.leftToRight(testArray);
+
+            let { value, done } = generator.next();
+            assert(value.length === 0);
+            assert(done);
+        });
+
+        it('should yield an array with ones turned to zeroes from left to right', () => {
+            let testArray = [1, 1, 1];
+            let generator = Reveal.leftToRight(testArray);
+            assert(generator.next().value.join('') === [0, 1, 1].join(''));
+            assert(generator.next().value.join('') === [0, 0, 1].join(''));
+            assert(generator.next().value.join('') === [0, 0, 0].join(''));
+        });
+
+        it('should yield an array at each step even when the bit for that step is already revealed', () => {
+            let testArray = [0, 1, 1];
+            let generator = Reveal.leftToRight(testArray);
+            assert(generator.next().value.join('') === [0, 1, 1].join(''));
+        });
+
+        it('should return an array early if all values are already zeroes', () => {
+            let testArray = [0, 0, 0];
+            let generator = Reveal.leftToRight(testArray);
+            let { value, done } = generator.next();
+            assert(value);
+            assert(done);
+        })
+    });
 });
 
