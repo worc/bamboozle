c342a2ffe3a3630432366a6fc72c17f6bb536e2c
worc <curtis.s.mcallister@gmail.com>
2018-02-18 16:02:11 -0800

    add leftToRight strategy, reformat to 4-space indenting

--- src/strategies/Reveal.js
+++ src/strategies/Reveal.js
@@ -8,57 +8,72 @@ import shuffle from '../util/Shuffle';
  * zeroes and ones indicate revealed and obfuscated characters
  *
  * a return statement is used when the bitmap is fully revealed in order to
- * end the generator *and* yield a defined value to the caller. 
+ * end the generator *and* yield a defined value to the caller.
  *
- * you see, the MDN generator function example had a yield as the last line in a generator, 
- * but that leads to ambiguity where you have to call the generator one more time 
+ * you see, the MDN generator function example had a yield as the last line in a generator,
+ * but that leads to ambiguity where you have to call the generator one more time
  * than necessary, then immediately recall the previous value. otherwise you're stuck
  * with the final yield which gives you { done: true, value: undefined } -_-
  *
  * on the other hand, a return statement in a generator will give you something
  * much more useful: { done: true, value: <a value!> } ^_^
- * 
+ *
  * so given that, yields are used while looping and returns are used when finalizing
  */
 export default class Reveal {
-  static *oneBitAndShuffleUntilDone(bitmap = []) {
-    let result = [];
-    let revealed = 0;
-    let obfuscated = 0;
+    static* oneBitAndShuffleUntilDone(bitmap = []) {
+        let result = [];
+        let revealed = 0;
+        let obfuscated = 0;
 
-    // first count up our ones/obfuscated and zeroes/revealed to initialize
-    bitmap.forEach(bit => {
-      // coercing a truthy here, which i guess leaves open the possibility
-      // of non-binary bits if you want to get real creative down the line
-      if(bit) {
-        obfuscated++;
-      } else {
-        revealed++;
-      }
-    });
+        // first count up our ones/obfuscated and zeroes/revealed to initialize
+        bitmap.forEach(bit => {
+            // coercing a truthy here, which i guess leaves open the possibility
+            // of non-binary bits if you want to get real creative down the line
+            if (bit) {
+                obfuscated++;
+            } else {
+                revealed++;
+            }
+        });
 
-    // so long as we have an array to work with, stay in this loop
-    while(bitmap) {
-      obfuscated--;
-      revealed++;
+        // so long as we have an array to work with, stay in this loop
+        while (bitmap) {
+            obfuscated--;
+            revealed++;
 
-      // not the best guard statements, but i'm already upside down with
-      // decrementing at the beginning of the loop... oh well...
-      obfuscated = (obfuscated < 0) ? 0 : obfuscated;
-      revealed = (revealed > bitmap.length) ? bitmap.length : revealed;
+            // not the best guard statements, but i'm already upside down with
+            // decrementing at the beginning of the loop... oh well...
+            obfuscated = (obfuscated < 0) ? 0 : obfuscated;
+            revealed = (revealed > bitmap.length) ? bitmap.length : revealed;
 
-      // a bit terse and ugly, but just building a new bitmap based on
-      // our current count of obfuscated and revealed (obfuscation assumed as ones here)
-      result = shuffle(Array(revealed).fill(0).concat(Array(obfuscated).fill(1)));
+            // a bit terse and ugly, but just building a new bitmap based on
+            // our current count of obfuscated and revealed (obfuscation assumed as ones here)
+            result = shuffle(Array(revealed).fill(0).concat(Array(obfuscated).fill(1)));
 
-      // finally, only return and indicate that we're done draining the ones
-      // if that's actually true :P
-      if(obfuscated > 0) {
-        yield result;
-      } else {
-        return result;
-      }
-    } 
-  }
+            // finally, only return and indicate that we're done draining the ones
+            // if that's actually true :P
+            if (obfuscated > 0) {
+                yield result;
+            } else {
+                return result;
+            }
+        }
+    }
+
+    static* leftToRight(bitmap = []) {
+        for (let i = 0; i < bitmap.length; i++) {
+            bitmap[i] = 0;
+
+            if (bitmap.includes(1)) {
+                yield bitmap;
+            } else {
+                return bitmap;
+            }
+        }
+
+        // zero-length fallthrough:
+        return bitmap;
+    }
 }
 
