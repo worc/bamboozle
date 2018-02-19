04244f9a46051422d2ba21767bac35769de9a674
worc <curtis.s.mcallister@gmail.com>
2018-02-18 16:52:29 -0800

    add Obfuscate.oneBitAndShuffleUntilDone strategy with tests

---
+++ src/strategies/Obfuscate.js
@@ -0,0 +1,49 @@
+import shuffle from '../util/Shuffle';
+
+/**
+ * when obfuscating we have a couple of options when the bitmap is
+ * full up with ones. we can either stop and return the last bitmap,
+ * or we can loop forever.
+ *
+ * looping forever let's the next function down the line treat all generators
+ * as potential infinite loops. it's a generalization or simplification that
+ * cuts down on decision making in the bitmap consumer.
+ *
+ * loops that and and return are suffixed with UntilDone, loops that continue yielding
+ * after the bitmap is full up are suffixed with Forever.
+ */
+export default class Obfuscate {
+    static* oneBitAndShuffleUntilDone(bitmap = []) {
+        let result = [];
+        let revealed = 0;
+        let obfuscated = 0;
+
+        bitmap.forEach(bit => {
+            if(bit) {
+                obfuscated++;
+            } else {
+                revealed++;
+            }
+        });
+
+        while(bitmap) {
+            // step
+            obfuscated++;
+            revealed--;
+
+            // guard
+            obfuscated = (obfuscated > bitmap.length) ? bitmap.length : obfuscated;
+            revealed = (revealed < 0) ? 0 : revealed;
+
+            // resolve
+            result = shuffle(Array(revealed).fill(0).concat(Array(obfuscated).fill(1)));
+
+            // yield/return
+            if(result.includes(0)) {
+                yield result;
+            } else {
+                return result;
+            }
+        }
+    }
+}
---
+++ test/strategies/ObfuscateTest.js
@@ -0,0 +1,28 @@
+import assert from 'assert';
+import Obfuscate from '../../src/strategies/Obfuscate';
+
+describe('Obfuscate', () => {
+    describe('#oneBitAndShuffleUntilDone', () => {
+        it('should return when all bits are ones', () => {
+            let testArray = [1, 1, 1];
+            let generator = Obfuscate.oneBitAndShuffleUntilDone(testArray);
+            let { value, done } = generator.next();
+            assert(value);
+            assert(done)
+        });
+
+        it('should return an empty array when given an empty array', () => {
+            let generator = Obfuscate.oneBitAndShuffleUntilDone([]);
+            let { value, done } = generator.next();
+            assert(value);
+            assert(done);
+        });
+
+        it('should yield an array with one more one on each step', () => {
+            let generator = Obfuscate.oneBitAndShuffleUntilDone([0, 0, 0]);
+            assert(generator.next().value.join('').match(/1/g).length === 1, 'first yield with 1 ones');
+            assert(generator.next().value.join('').match(/1/g).length === 2, 'second yield with 2 ones');
+            assert(generator.next().value.join('').match(/1/g).length === 3, 'third and last return with 3 ones');
+        })
+    });
+});
