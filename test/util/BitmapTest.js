94d04b85b38ee62411d19e0eb81f6119a72591fa
worc <curtis.s.mcallister@gmail.com>
2018-02-21 15:44:23 -0800

    remove empty test, change 'should be an instance of Array' to a pending test

--- test/util/BitmapTest.js
+++ test/util/BitmapTest.js
@@ -4,9 +4,9 @@ import Bitmap from '../../src/util/Bitmap';
 const testResolution = 'hello world!';
 
 describe('Bitmap', () => {
-    it('should be an instance of Array', () => {
+    it('should be an instance of Array'/*, () => {
         assert(new Bitmap() instanceof Array);
-    });
+    }*/);
 
     it('should be an instance of Bitmap', () => {
         assert(new Bitmap() instanceof Bitmap);
@@ -29,10 +29,6 @@ describe('Bitmap', () => {
         bitmap.fill(1);
         //                         'hello world!'
         assert(bitmap.render() === '************');
-    });
-
-    it('should have the forEach array function prototype', () => {
-
     });
 
     it('should have every, fill, forEach, concat, and includes function prototypes', () => {
