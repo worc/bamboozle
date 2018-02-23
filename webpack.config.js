7d1fb3f17481680642ecb586b9526b5abb6a98a0
worc <curtis.s.mcallister@gmail.com>
2018-02-22 16:11:36 -0800

    add build task to create a minimized, web-ready bundle

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const packageJSON = require('./package.json');
+
+const banner = `${packageJSON.name} ${packageJSON.version} - ${packageJSON.description}\nCopyright 2018 ${packageJSON.author} - ${packageJSON.homepage}\nLicense: ${packageJSON.license}`;
+
+module.exports = {
+    context: __dirname + '/src',
+    entry: './Bamboozle.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: `${packageJSON.name}.min.js`,
+        library: `${packageJSON.name}`,
+        // libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
+        new webpack.BannerPlugin(banner)
+    ]
+};
