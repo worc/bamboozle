08210a9f36c87be8591794216de62a939540a764
worc <curtis.s.mcallister@gmail.com>
2018-03-06 13:19:52 -0800

    add stub and note to TaskRunnerTest

--- test/util/TaskRunnerTest.js
+++ test/util/TaskRunnerTest.js
@@ -1,13 +1,62 @@
 import assert from 'assert';
 import TaskRunner from '../../src/util/TaskRunner';
+import Bitmap from '../../src/util/Bitmap';
+
+const resolution = 'hello world!';
+const testGenerator = {
+    next: () => {
+        return {
+            value: [],
+            done: true
+        }
+    }
+};
 
 describe('TaskRunner', () => {
+    let testBitmap = new Bitmap(resolution);
+
     describe('#constructor', () => {
-        it('should have sane defaults', () => {
-            let taskRunner = new TaskRunner();
-            assert(taskRunner.bitmap);
-            assert(taskRunner.options);
-            assert(taskRunner.listener);
+        let taskRunner = new TaskRunner(testBitmap);
+
+        it('should have defaults', () => {
+            assert(taskRunner.queue instanceof Array);
+            assert(taskRunner.stopped === true);
+        });
+
+        it('should have the add, addLoop, addSingleRun, play, run, step, stop endpoints', () => {
+            assert(taskRunner.add);
+            assert(taskRunner.addLoop);
+            assert(taskRunner.addSingleRun);
+            assert(taskRunner.play);
+            assert(taskRunner.run);
+            assert(taskRunner.step);
+            assert(taskRunner.stop);
+        });
+
+        it('should emit the initial state when constructed', (done) => {
+            let listener = (message) => {
+                assert(message === 'test');
+                done();
+            };
+
+            let taskRunner = new TaskRunner(new Bitmap('test'), 20, listener);
         });
     });
+
+    describe('#step', () => {
+        let testTaskRunner = new TaskRunner(testBitmap);
+
+       it('should return a promise', (done) => {
+           assert(testTaskRunner.step(testGenerator, 100) instanceof Promise);
+           done();
+       });
+    });
+
+    describe('#stop', () => {
+        it('causes infinite recursion if it calls .updateListener() and that listener also calls .stop()');
+    });
+
+    describe('#updateListener', () => {
+        it('should have tests...');
+    })
 });
